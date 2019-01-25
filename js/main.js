c8a046efdaa9053ea67a2837a6b81ee8f1122b67
tanloo <zxc5095@gmail.com>
2019-01-25 14:46:38 +0800

    ES6

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function showSymbolEditDiv(self) {
     //获取点击的SVG
-    var originSVG = $(self).children(':first');
-
+    const originSVG = $(self).children(':first');
+    let slider;
     //判断是线或非线
     if ($(self).attr('id') === 'symbol_line') {
         //隐藏非线条件面板等
@@ -19,7 +19,7 @@ function showSymbolEditDiv(self) {
         let symbolSize = originSVG.children(':first').attr('stroke-width');
         symbolSize = Number(symbolSize);
         $('#sliderVal_shape').text(symbolSize);
-        var s = $('#slider_shape').slider({
+        slider = $('#slider_shape').slider({
             min: 0,
             max: 18,
             value: symbolSize
@@ -42,13 +42,13 @@ function showSymbolEditDiv(self) {
         }
         symbolSize = Number(symbolSize);
         $('#sliderVal_shape').text(symbolSize);
-        var s = $('#slider_shape').slider({
+        slider = $('#slider_shape').slider({
             min: 0,
             max: 120,
             value: symbolSize
         });
     }
-    s.on("slide", sliderChange_shape);
+    slider.on("slide", sliderChange_shape);
     $('#slider_outline').slider();
     $("#slider_outline").on("slide", sliderChange_line);
 
@@ -87,13 +87,12 @@ function showSymbolEditDiv(self) {
         yes: function (index, layero) {
             //用弹出层中调节好的图形替换页面中图形
             if ($(self).attr('id') === 'symbol') {
-                var previewSymbol = $('#symbolPreview');
+                let previewSymbol = $('#symbolPreview');
                 $('#symbol').html(previewSymbol.html());
             } else {
-                var previewSymbol = $('#symbolPreview_line');
+                let previewSymbol = $('#symbolPreview_line');
                 $('#symbol_line').html(previewSymbol.html());
             }
-            s.slider('destroy');
             layer.close(index);//需手动关闭弹出层
         }
     });
@@ -116,31 +115,32 @@ function showSymbolEditDiv(self) {
             "url(#previewLinearGradientItem)");
 
         //将色带居中的颜色填充至预图形中
-        var selectedItemColor = $(
+        let selectedItemColor = $(
             '#selectedlinearGradientItem > svg > defs > linearGradient stop:nth-child(1)'
         ).attr('stop-color');
         $('#symbolPreview > svg').children(':first').attr("fill", selectedItemColor);
         $('#symbolPreview_line > svg').children(':first').attr("stroke", selectedItemColor);
     })
 
-    $('.shapeItem').click(function (a) {
+    $('.shapeItem').on('click', function (a) {
+
         switch ($(this).attr("id")) {
-            case 'circleShape':
-                var svg = $('#symbolPreview > svg');
+            case 'circleShape': {
+                let svg = $('#symbolPreview > svg');
                 //判断是否从矩形菱形更改过来
-                if (svg.html().substr(1, 4) === "path") {
+                if (svg.children(':first').prop("tagName") === "path") {
                     //添加圆心坐标属性
                     svg.children(':first').attr('cx', "0");
                     svg.children(':first').attr('cy', "0");
                     //用矩形菱形边长设置圆半径
-                    var d = svg.children(':first').attr('d');
+                    let d = svg.children(':first').attr('d');
                     svg.children(':first').attr('r', Math.abs(d.split(" ")[1]));
                     //移除不属于圆的属性
                     svg.children(':first').removeAttr('path');
                     svg.children(':first').removeAttr('d');
                     //替换标签名
-                    var circleNode = svg.children(':first');
-                    var html = svg.html();
+                    let circleNode = svg.children(':first');
+                    let html = svg.html().trim();
                     html = html.replace(/^<path/, "<circle");
                     html = html.replace(/\/path>$/, "/circle>");
 
@@ -149,45 +149,51 @@ function showSymbolEditDiv(self) {
                     break;
                 }
                 break;
-            case 'rectShape':
-                var svg = $('#symbolPreview > svg');
+            }
+
+            case 'rectShape': {
+                let svg = $('#symbolPreview > svg');
                 //直接取r属性，若r属性不存在则来自于菱形
-                var r = svg.children(':first').attr('r');
+                let r = svg.children(':first').attr('r');
                 if (r === undefined) {
                     r = Math.abs(svg.children(':first').attr("d").split(" ")[1]);
                 }
                 //构造d属性结构，并替换至展示图形中
-                var d = "M -" + r + " " + r + " L -" + r + " -" + r + " L " + r + " -" + r + " L " + r + " " + r + " L -" + r + " " + r + " Z";
+                let d = "M -" + r + " " + r + " L -" + r + " -" + r + " L " + r + " -" + r + " L " + r + " " + r + " L -" + r + " " + r + " Z";
                 svg.children(':first').attr('d', d);
                 //移除不属于矩形图形的属性
                 svg.children(':first').removeAttr('cx');
                 svg.children(':first').removeAttr('cy');
                 svg.children(':first').removeAttr('r');
                 //替换标签名
-                var circleNode = svg.children(':first');
-                var html = svg.html();
+                let circleNode = svg.children(':first');
+                let html = svg.html();
                 html = html.trim().replace(/^<circle/, "<path");
                 html = html.replace(/\/circle>$/, "/path>");
 
                 svg.html(html);
                 break;
-            case 'diamondShape':
-                var svg = $('#symbolPreview > svg');
-                var r = svg.children(':first').attr('r');
+            }
+
+            case 'diamondShape': {
+                let svg = $('#symbolPreview > svg');
+                let r = svg.children(':first').attr('r');
                 if (r === undefined) {
                     r = Math.abs(svg.children(':first').attr("d").split(" ")[1]);
                 }
-                var d = "M -" + r + " " + 0 + " L -" + 0 + " -" + r + " L " + r + " -" + 0 + " L " + 0 + " " + r + " L -" + r + " " + 0 + " Z";
+                let d = "M -" + r + " " + 0 + " L -" + 0 + " -" + r + " L " + r + " -" + 0 + " L " + 0 + " " + r + " L -" + r + " " + 0 + " Z";
                 svg.children(':first').attr('d', d);
                 svg.children(':first').removeAttr('cx');
                 svg.children(':first').removeAttr('cy');
                 svg.children(':first').removeAttr('r');
-                var circleNode = svg.children(':first');
-                var html = svg.html();
+                let circleNode = svg.children(':first');
+                let html = svg.html();
                 html = html.trim().replace(/^<circle/, "<path");
                 html = html.replace(/\/circle>$/, "/path>");
                 svg.html(html);
                 break;
+            }
+
         }
         //用背景色模拟选中效果
         $('.shapeItem').css('background-color', '#FFFFFF');
@@ -229,7 +235,7 @@ function showSymbolEditDiv(self) {
                 }
             }
         }).on('colorpickerChange colorpickerCreate', function (e) {
-            var colors = e.color.toString();
+            let colors = e.color.toString();
             //动态显示预图形颜色
             $('#symbolPreview > svg').children(':first').attr("fill", colors);
             $('#symbolPreview_line > svg').children(':first').attr("stroke", colors);
@@ -268,7 +274,7 @@ function showSymbolEditDiv(self) {
                 }
             }
         }).on('colorpickerChange colorpickerCreate', function (e) {
-            var colors = e.color.toString();
+            let colors = e.color.toString();
             //设置轮廓颜色
             $('#symbolPreview > svg').children(':first').attr("stroke", colors);
         });
@@ -288,7 +294,7 @@ function setMatrix(a, b, c, d, e, f) {
 
 //根据比例增减path中的d属性，模式如"M -50 -50 L 50 -50 L 50 50 L -50 50 L -50 -50"
 function getPath_d(origin, ratio) {
-    var tempArr = origin.split(" ");
+    let tempArr = origin.split(" ");
     tempArr[1] = Number(tempArr[1]) - ratio;
     tempArr[5] = Number(tempArr[5]) - ratio;
     tempArr[7] = Number(tempArr[7]) + ratio;
@@ -314,18 +320,18 @@ function getPath_d(origin, ratio) {
  */
 function sliderChange_shape(slideEvt) {
     //获取图形SVG类型
-    var shapeType = $('#symbolPreview > svg').html().trim().substr(1, 4);
+    const shapeType = $('#symbolPreview > svg').children(':first').prop('tagName');
     //从span标签中取出滑块改变前的值
-    var currentSliderValue = Number($("#sliderVal_shape").text());
+    const currentSliderValue = Number($("#sliderVal_shape").text());
     //改变后值 减去 改变前的值得到变化量，绝对值以适应增减
-    var sliderChangedValue = Math.abs(slideEvt.value - currentSliderValue);
-    var originHeight = Number($('#symbolPreview > svg').attr('height'));
-    var originWidth = Number($('#symbolPreview > svg').attr('width'));
-    var originMatrix = eval("get" + $('#symbolPreview > svg').children(':first').attr("transform"));
+    const sliderChangedValue = Math.abs(slideEvt.value - currentSliderValue);
+    const originHeight = Number($('#symbolPreview > svg').attr('height'));
+    const originWidth = Number($('#symbolPreview > svg').attr('width'));
+    const originMatrix = eval("get" + $('#symbolPreview > svg').children(':first').attr("transform"));
     //判断为矩形菱形或圆
     if (shapeType === "path") {
         //获取图形当前path绘制路径量
-        var d = $('#symbolPreview > svg').children(':first').attr("d");
+        const d = $('#symbolPreview > svg').children(':first').attr("d");
         if (slideEvt.value > currentSliderValue) {
             //滑块增加
             //让图形以0.5的像素单位变化增长
@@ -344,7 +350,7 @@ function sliderChange_shape(slideEvt) {
             $('#symbolPreview > svg').attr('height', originHeight - sliderChangedValue);
         }
     } else {
-        var r = Number($('#symbolPreview > svg').children(':first').attr('r'));
+        const r = Number($('#symbolPreview > svg').children(':first').attr('r'));
         if (slideEvt.value > currentSliderValue) {
             $('#symbolPreview > svg').children(':first').attr('r', r + sliderChangedValue * 0.5);
             $('#symbolPreview > svg').children(':first').attr("transform", setMatrix(originMatrix[0], originMatrix[1], originMatrix[2], originMatrix[3], originMatrix[4] + sliderChangedValue * 0.5, originMatrix[5] + sliderChangedValue * 0.5));
@@ -361,9 +367,9 @@ function sliderChange_shape(slideEvt) {
     if (!$('#symbolPreview_line').hasClass('hidden')) {
         //更新线宽度
         $('#symbolPreview_line > svg > path').attr('stroke-width', slideEvt.value);
-
+        const lineType = $('#lineSelect').val();
         //改变预图形中的线型
-        switch ($('#lineSelect').val()) {
+        switch (lineType) {
             //实线
             case 'solid':
                 break;
@@ -392,11 +398,11 @@ function sliderChange_shape(slideEvt) {
 
 function sliderChange_line(slideEvt) {
     $('#symbolPreview > svg').children(':first').attr("stroke-width", slideEvt.value);
-    var originHeight = Number($('#symbolPreview > svg').attr('height'));
-    var originWidth = Number($('#symbolPreview > svg').attr('width'));
-    var currentSliderValue = Number($("#sliderVal_outline").text());
-    var sliderChangedValue = Math.abs(slideEvt.value - currentSliderValue);
-    var originMatrix = eval("get" + $('#symbolPreview > svg').children(':first').attr("transform"));
+    const originHeight = Number($('#symbolPreview > svg').attr('height'));
+    const originWidth = Number($('#symbolPreview > svg').attr('width'));
+    const currentSliderValue = Number($("#sliderVal_outline").text());
+    const sliderChangedValue = Math.abs(slideEvt.value - currentSliderValue);
+    const originMatrix = eval("get" + $('#symbolPreview > svg').children(':first').attr("transform"));
 
     if (slideEvt.value > currentSliderValue) {
         $('#symbolPreview > svg').attr('height', originHeight + sliderChangedValue * 1.5);
@@ -416,27 +422,29 @@ function sliderChange_line(slideEvt) {
 $(document).ready(function () {
 
     //请求色带信息
-    var xmlhttp = new window.XMLHttpRequest();
-    xmlhttp.open("GET", "./assets/svg.xml", false);
-    xmlhttp.send(null);
-    if (xmlhttp.readyState == 4) {
-        xmlDoc = xmlhttp.responseXML.documentElement
-    }
-    var svgs = xmlDoc.getElementsByTagName("svg");
-    var svgsHTML = "";
-    for (var svg of svgs) {
-        var h = '<div class="linearGradientItem">' + $(svg).get(0).outerHTML + '</div>';
-        svgsHTML += h;
-    }
-    $('#linearGradientItems').html(svgsHTML);
+    (() => {
+        let xmlhttp = new window.XMLHttpRequest();
+        xmlhttp.open("GET", "./assets/svg.xml", false);
+        xmlhttp.send(null);
+        if (xmlhttp.readyState == 4) {
+            xmlDoc = xmlhttp.responseXML.documentElement
+        }
+        const svgs = xmlDoc.getElementsByTagName("svg");
+        let svgsHTML = "";
+        for (let svg of svgs) {
+            let h = '<div class="linearGradientItem">' + $(svg).get(0).outerHTML + '</div>';
+            svgsHTML += h;
+        }
+        $('#linearGradientItems').html(svgsHTML);
+    })();
 
 
     //更改线型显示
     $('#lineSelect').change(function () {
-        var lineType = $(this).val();
+        const lineType = $(this).val();
         $('#lineImg').css('background-position', '0px -' + ($('#lineSelect').get(0).selectedIndex * 15) + 'px');
 
-        var lineWidth = $('#sliderVal_shape').text();
+        const lineWidth = $('#sliderVal_shape').text();
         switch (lineType) {
             case 'solid':
                 break;
